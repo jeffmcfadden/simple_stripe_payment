318fe3aace91ec2a545e1b4c66f6bb0f538badef
Jeff McFadden <jeff.mcfadden@gmail.com>
2012-03-01 10:59:00 -0700

    Heroku deploy change.

--- config/application.rb
+++ config/application.rb
@@ -52,6 +52,9 @@ module SimplePayment
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    # Heroku
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
