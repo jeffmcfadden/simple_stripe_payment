45659cccd42756c44da6fca2fab142542c4e7883
Jeff McFadden <jeff.mcfadden@gmail.com>
2012-03-01 09:22:41 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# A Simple Stripe.com Payment App
+
+Fill out a form, charge the card, save the payment. Done.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimplePayment::Application.config.secret_token = '1f48fafda7efc43c1592e5800bc863956b34a91bf9ecbe701a1d10924537458da085968b0e30115e48724335c79029693aa0a8e07bd84e7850a1fb72202a1458'
